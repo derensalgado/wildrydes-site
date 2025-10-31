06e721dc4bcf5e76084b3e7e23c9d43b0be47218
derensalgado <68981734+derensalgado@users.noreply.github.com>
2025-10-30 21:16:13 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-1_EjjZA8NRC', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '294nhucpjtmhselcgu8eh3naku', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-1_Zee3KwH1d', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '24qtm5th6cj6a9jhnparlkopno', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
